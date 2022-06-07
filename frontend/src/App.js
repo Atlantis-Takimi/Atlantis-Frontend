376c55332958040a0ab442478d5ada68e4807ce5
bilginmrv <47225405+mervebilgin@users.noreply.github.com>
2022-06-07 09:11:29 +0300

    Update App.jsx folder

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import Container from './Components/Container/Container';
-import Menu from './Components/Menu/Menu';
+import Menu from './Components/Menu/Menu'
+
 
 function App() {
   return (
@@ -11,4 +12,37 @@ function App() {
   );
 }
 
+// import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+// import Academics from './Pages/Academics'
+// import Events from './Pages/Events'
+// import HelpCenter from './Pages/HelpCenter'
+// import Lessons from './Pages/Lessons'
+// import Profile from './Pages/Profile'
+// import QuestionsAnswers from './Pages/QuestionsAnswers'
+// import Settings from './Pages/Settings' 
+
+
+// function App() {
+//   return (
+
+//     <>
+//       <BrowserRouter>
+//           <Menu/>
+//           <Container/>
+//           <Routes>
+//             <Route path='/' exact element={<Profile/>}/>
+//             <Route path='/academics' element={<Academics/>}/>
+//             <Route path='/events' element={<Events/>}/>
+//             <Route path='/helpCenter' element={<HelpCenter/>}/>
+//             <Route path='/lessons' element={<Lessons/>}/> 
+//             <Route path='/questionsAnswers' element={<QuestionsAnswers/>}/>
+//             <Route path='/settings' element={<Settings/>}/>
+//           </Routes>
+//       </BrowserRouter>
+//     </>
+//   );
+// } 
+
 export default App;
+
