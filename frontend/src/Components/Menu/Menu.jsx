ec87ad10dd9cca0b4e461349c4be4c0cbb88cd6f
bilginmrv <47225405+mervebilgin@users.noreply.github.com>
2022-06-08 21:53:50 +0300

    Update Menu folder

--- frontend/src/Components/Menu/Menu.jsx
+++ frontend/src/Components/Menu/Menu.jsx
@@ -1,8 +1,58 @@
 import React, {useEffect} from 'react'
 import './Menu.css'
-import Logo from '../../img/logo.png'
+// import Logo from '../../img/logo.png'
 import {FaDelicious, FaEnvelopeOpenText, FaGraduationCap, FaCalendar, FaCog, FaSignOutAlt} from 'react-icons/fa';
 import {AiFillBook, AiFillQuestionCircle} from "react-icons/ai";
+
+// function Menu() {
+
+  // useEffect(() =>{
+  //   const mainMenuLi = document
+  //     .getElementById("mainMenu")
+  //     .querySelectorAll("li");
+
+  //   function changeActive() {
+  //     mainMenuLi.forEach(n => n.classList.remove("active"));
+  //     this.classList.add("active");
+  //   }
+
+  //   mainMenuLi.forEach(n => n.addEventListener('click', changeActive ))
+  // }, []);
+
+//   return (
+//     <menu>
+//         <img src={Logo} alt=''/>
+
+        // <ul id='mainMenu' >
+      
+        //   <Icon icon={<FaDelicious />}/>
+        //   <Icon icon={<AiFillBook/>}/>
+        //   <Icon icon={<FaEnvelopeOpenText/>}/>
+        //   <Icon icon={<FaGraduationCap/>}/>
+        //   <Icon icon={<FaCalendar/>}/>
+        //   <Icon icon={<AiFillQuestionCircle/>}/>
+        // </ul>
+
+        // <ul className='lastMenu'>
+        //   <Icon icon={<FaCog/>}/>
+        //   <Icon icon={<FaSignOutAlt/>}/>
+        // </ul>
+//     </menu>
+//   )
+// }
+
+// const Icon = ({icon}) => (
+//     <li>
+//         <a href='/academics'>{icon}</a>
+//     </li>
+// );
+
+// export default Menu
+
+
+import logo from '../../img/logo.png';
+import {Link} from 'react-router-dom';
+
 
 function Menu() {
 
@@ -18,65 +68,49 @@ function Menu() {
 
     mainMenuLi.forEach(n => n.addEventListener('click', changeActive ))
   }, []);
-
-  return (
+  
+  return(
     <menu>
-        <img src={Logo} alt=''/>
+      <Link className='' to="/">
+        <img src={logo} alt=''/>
+        {/* <img src={logo} width='70' alt=''></img> */}
+      </Link>
+        
+      <ul id='mainMenu' > 
+        <li>
+          <a href='/mainContainer' exact element>{<FaDelicious />}</a>
+        </li>
+        <li>
+          <a href='/lessons'>{<AiFillBook/>}</a>
+        </li>
+        <li>
+          <a href='/questionsAnswers'>{<FaEnvelopeOpenText/>}</a>
+        </li>
+        <li>
+          <a href='/academics'>{<FaGraduationCap/>}</a>
+        </li>
+        <li>
+          <a href='/events'>{<FaCalendar/>}</a>
+        </li>
+        <li>
+          <a href='/helpCenter'>{<AiFillQuestionCircle/>}</a>
+        </li>
+      </ul>
 
-        <ul id='mainMenu' >
-      
-          <Icon icon={<FaDelicious />}/>
-          <Icon icon={<AiFillBook/>}/>
-          <Icon icon={<FaEnvelopeOpenText/>}/>
-          <Icon icon={<FaGraduationCap/>}/>
-          <Icon icon={<FaCalendar/>}/>
-          <Icon icon={<AiFillQuestionCircle/>}/>
-        </ul>
-
-        <ul className='lastMenu'>
-          <Icon icon={<FaCog/>}/>
-          <Icon icon={<FaSignOutAlt/>}/>
-        </ul>
+      <ul className='lastMenu'>
+        <li>
+          <a href='/settings'>{<FaCog/>}</a>
+        </li>
+        <li>
+          <a href='/logOut'>{<FaSignOutAlt/>}</a>
+        </li>
+      </ul>
+       
     </menu>
-  )
+    )
 }
 
-const Icon = ({icon}) => (
-    <li>
-        <a href='/academics'>{icon}</a>
-    </li>
-);
-
 export default Menu
-
-
-// import {Navbar, Nav} from 'react-bootstrap';
-// import logo from '../../img/logo.png';
-// import {Link} from 'react-router-dom';
-
-// function Menu() {
-//     return(
-//       // <Navbar className='backdrop' variant="dark" expand="lg" >
-//       <menu>
-//         <img src={logo} alt=''/>
-//         <Link className='' to="/">
-//           <img className='navbar-logo' src={logo} width='70' alt='logo'></img>
-//           <span>Fütag Dergi</span>
-//         </Link>
-//         <Navbar.Toggle aria-controls="basic-navbar-nav" />
-//         <Navbar.Collapse id="basic-navbar-nav">
-//           <Nav className="flex-sm-column">
-//             <Link className='nav-link' to="/">Ana Sayfa</Link>
-//             <Nav.Link className='nav-link' href="/academics">Sayılar</Nav.Link>
-//             <Nav.Link className='nav-link' href="/#spns">Sponsorlar</Nav.Link>
-//             <Link className='nav-link' to="/hakkimizda">Hakkımızda</Link>
-//             <Nav.Link className='nav-link' href="#contact">İletişim</Nav.Link>
-//           </Nav>
-//         </Navbar.Collapse>
-//       </menu>
-//     )
-// }
-// export default Menu
 
 /*
 return (
@@ -99,11 +133,12 @@ return (
         </ul>
     </menu>
   )
-}
+}*/
 
-const Icon = ({icon}) => (
-    <li>
-        <a href='#xd'>{icon}</a>
-    </li>
-);
-*/
+// const Icon = ({icon1}) => (
+//     <li>
+//         <a href='#xd'>{icon1}</a>
+//     </li>
+
+// );
+
