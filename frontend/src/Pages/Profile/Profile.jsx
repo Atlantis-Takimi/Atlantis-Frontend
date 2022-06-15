6d379614f38cfdad285e266efb1ea693d7fc132d
bilginmrv <mrvblgn44@gmail.com>
2022-06-15 13:04:54 +0300

    Edit profile page contact section

--- frontend/src/Pages/Profile/Profile.jsx
+++ frontend/src/Pages/Profile/Profile.jsx
@@ -2,7 +2,9 @@ import React from 'react'
 import './profile.css'
 import img from '../../img/student.jpg';
 import {BsMouse} from 'react-icons/bs'
-
+import {AiOutlineLinkedin} from 'react-icons/ai'
+import {AiOutlineMail} from 'react-icons/ai'
+import {AiOutlineGithub} from 'react-icons/ai'
 
 
 function Profile() {
@@ -19,7 +21,7 @@ function Profile() {
       </div>
 
       <div id='home' className='container home-container'>
-        <div className='logo'>
+        {/* <div className='logo'>
           <div className="main-img">
             <span className="circle"></span>
             <span className="circle"></span>
@@ -31,8 +33,8 @@ function Profile() {
             <span className="circle"></span>
           </div>
 
-          {/* <img src={img} alt='' /> */}
-        </div>
+          <img src={img} alt='' />
+        </div> */}
 
         <a href="#footer" className="scroll-down">
           <hr/>
@@ -49,18 +51,38 @@ function Profile() {
           </p>
         </h2>
 
-        <div className='container button-container'>
+        <div className='button-container'>
           <a href='#about' className='btn-pri'>
-            Learn more
-          </a>
-          <a href='#contact' className='btn-sec'>
-            Get in touch
+            Düzenle
           </a>
         </div>
       </div>
 
-      
-      
+      {/* <div id='about' className='container about-container'>
+
+      </div> */}
+
+      {/* Contact */}
+
+      <div className='container contact-container'>
+        <h1>İletişim Bilgilerim</h1>
+          <div className='contact-links'>
+            <a href='#linkedin' className='contact youtube'>
+              <AiOutlineLinkedin  className='icon'/>
+              <h2>linkedin <span>Yasmin İlk</span></h2>
+            </a>
+
+            <a href='#posta' className='contact mail'>
+              <AiOutlineMail  className='icon'/>
+              <h2>e-posta <span>yasemin@gmail.com</span></h2>
+            </a>
+
+            <a href='#github' className='contact github'>
+              <AiOutlineGithub  className='icon'/>
+              <h2>github <span>github.com/yasemin</span></h2>
+            </a>
+          </div>
+      </div>      
     </div>   
   )
 }
