dfb13e23d674093aa04fc0f47fd92ff91b75172b
bilginmrv <mrvblgn44@gmail.com>
2022-06-15 20:53:49 +0300

    Update help center page

--- frontend/src/Pages/HelpCenter/HelpCenter.jsx
+++ frontend/src/Pages/HelpCenter/HelpCenter.jsx
@@ -11,28 +11,28 @@ function HelpCenter() {
 				<div className="accordion-item">
 					<button id="accordion-button-5" aria-expanded="false"><span className="accordion-title">Temel Bilgiler Hakkında</span><span className="icon" aria-hidden="true"></span></button>
 					<div class="accordion-content">
-						<p>Evet. Araştırmalar şimdi online terapinin de aynı derecede etkili olabileceğini gösteriyor (hatta bazı ruh sağlığı sorunları için daha etkili olduğu bile görülüyor). Ayrıca çevrimiçi terapinin rahatlığı onu daha erişilebilir kılmaktadır.</p>
+						<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>
 					</div>
 				</div>
         
 				<div className="accordion-item">
 					<button id="accordion-button-5" aria-expanded="false"><span className="accordion-title">Temel Problemler Hakkında</span><span className="icon" aria-hidden="true"></span></button>
 					<div class="accordion-content">
-						<p>Evet. Araştırmalar şimdi online terapinin de aynı derecede etkili olabileceğini gösteriyor (hatta bazı ruh sağlığı sorunları için daha etkili olduğu bile görülüyor). Ayrıca çevrimiçi terapinin rahatlığı onu daha erişilebilir kılmaktadır.</p>
+						<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>
 					</div>
 				</div>
 
         <div className="accordion-item">
 					<button id="accordion-button-5" aria-expanded="false"><span className="accordion-title">Kullanıcı Girişi Hakkında</span><span className="icon" aria-hidden="true"></span></button>
 					<div class="accordion-content">
-						<p>Evet. Araştırmalar şimdi online terapinin de aynı derecede etkili olabileceğini gösteriyor (hatta bazı ruh sağlığı sorunları için daha etkili olduğu bile görülüyor). Ayrıca çevrimiçi terapinin rahatlığı onu daha erişilebilir kılmaktadır.</p>
+						<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>
 					</div>
 				</div>
 
         <div className="accordion-item">
 					<button id="accordion-button-5" aria-expanded="false"><span className="accordion-title">Gizlilik</span><span className="icon" aria-hidden="true"></span></button>
 					<div class="accordion-content">
-						<p>Evet. Araştırmalar şimdi online terapinin de aynı derecede etkili olabileceğini gösteriyor (hatta bazı ruh sağlığı sorunları için daha etkili olduğu bile görülüyor). Ayrıca çevrimiçi terapinin rahatlığı onu daha erişilebilir kılmaktadır.</p>
+						<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>
 					</div>
 				</div>
 
